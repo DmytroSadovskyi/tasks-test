c1ae0c8436cf88489ac4ad780867ed5c3370791c
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-10-11 13:07:24 +0300

    Update StatusFilter.jsx

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Button from "../Button";
 import css from "./StatusFilter.module.css";
 import { statusFilters } from "../../redux/constants";
@@ -9,27 +10,33 @@ import { setStatusFilter } from "../../redux/actions";
 const StatusFilter = () => {
   const filter = useSelector(getStatusFilter);
   const dispatch = useDispatch();
+
   const handleFilterChange = (filter) => {
     dispatch(setStatusFilter(filter));
-   
   };
 
   return (
     <div className={css.wrapper}>
       <Button
-        selected={filter === statusFilters.all}
+        className={
+          filter === statusFilters.all ? css.selectedButton : css.button
+        }
         onClick={() => handleFilterChange(statusFilters.all)}
       >
         All
       </Button>
       <Button
-        selected={filter === statusFilters.active}
+        className={
+          filter === statusFilters.active ? css.selectedButton : css.button
+        }
         onClick={() => handleFilterChange(statusFilters.active)}
       >
         Active
       </Button>
       <Button
-        selected={filter === statusFilters.completed}
+        className={
+          filter === statusFilters.completed ? css.selectedButton : css.button
+        }
         onClick={() => handleFilterChange(statusFilters.completed)}
       >
         Completed
